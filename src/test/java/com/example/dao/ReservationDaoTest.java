eda258f2751f6213f5702909600a8d8230c32229
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-05-30 11:04:01 +0900

    up

--- src/test/java/com/example/dao/ReservationDaoTest.java
+++ src/test/java/com/example/dao/ReservationDaoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +26,7 @@ class ReservationDaoTest {
 
     @BeforeEach
     void setUp() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "reservation");
     }
 
     @AfterEach
@@ -55,9 +57,13 @@ class ReservationDaoTest {
 
     @Test
     void selectById() {
-        var actual = dao.selectById(1);
+        var entity = new Reservation();
+        entity.name = "foo";
+        dao.insert(entity);
+
+        var actual = dao.selectById(entity.id);
         assertThat(actual).isPresent();
-        assertThat(actual.get()).extracting("id", "name").containsExactly(1, "foo");
+        assertThat(actual.get()).extracting("id", "name").containsExactly(entity.id, "foo");
     }
 
 }
