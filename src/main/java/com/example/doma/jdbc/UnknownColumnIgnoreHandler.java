01eed757a630ef9dd159c90cfe1596b4e156ca35
izuno4t <9846+izuno4t@users.noreply.github.com>
2026-05-20 23:13:11 +0900

    chore: add Checkstyle configuration and format code in UnknownColumnIgnoreHandler

--- src/main/java/com/example/doma/jdbc/UnknownColumnIgnoreHandler.java
+++ src/main/java/com/example/doma/jdbc/UnknownColumnIgnoreHandler.java
@@ -9,7 +9,8 @@ import java.util.function.Supplier;
 public final class UnknownColumnIgnoreHandler implements UnknownColumnHandler {
 
     @Override
-    public void handle(Query query, EntityType<?> entityType, String unknownColumnName, Supplier<String> informationSupplier) {
+    public void handle(Query query, EntityType<?> entityType, String unknownColumnName,
+                       Supplier<String> informationSupplier) {
         // do nothing
     }
 }
